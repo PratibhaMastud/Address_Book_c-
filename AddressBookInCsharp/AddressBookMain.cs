9365e3ea7b6317d6050ed693fb77c602986d2b0b
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-03 18:29:33 +0530

    [Pratibha Mastud] Add: Delete Contact details

--- AddressBookInCsharp/AddressBookMain.cs
+++ AddressBookInCsharp/AddressBookMain.cs
@@ -8,10 +8,11 @@ public class AddressBookMain
 		Console.WriteLine("            Welcome to Address Book          ");
 		Console.WriteLine("---------------------------------------------");
 		addressBookServices.addContact();
-		Console.WriteLine("Enter first name and last name of the person to edit the contact: ");
+		Console.WriteLine("Enter first name and last name of the person to for remove the contact: ");
 		String firstName = Console.ReadLine();
 		String lastName = Console.ReadLine();
-		addressBookServices.editContact(firstName, lastName);
+		addressBookServices.deleteContactDetail(firstName, lastName);
+
 	}
 
 }
--- AddressBookInCsharp/AddressBookServices.cs
+++ AddressBookInCsharp/AddressBookServices.cs
@@ -4,51 +4,41 @@ public class AddressBookServices
 {
 		Person person = new Person();
 
-		// TO ADD NEW CONTACT
-		public void addContact()
-		{
-			Console.WriteLine("Enter FirstName : ");
-			person.setFirstName(Console.ReadLine());
-			Console.WriteLine("Enter LastName : ");
-			person.setLastName(Console.ReadLine());
-			Console.WriteLine("Enter address : ");
-			person.setAddress(Console.ReadLine());
-			Console.WriteLine("Enter City  : ");
-			person.setCity(Console.ReadLine());
-			Console.WriteLine("Enter state  : ");
-			person.setState(Console.ReadLine());
-			Console.WriteLine("Enter Mobile Number:");
-			person.setPhoneNumber(Console.ReadLine());
-			Console.WriteLine("Enter Email Id : ");
-			person.setEmail(Console.ReadLine());
-
-			Console.WriteLine(person);
-		}
-
-		// EDIT CONTACT DETAIL BY PERSON NAME
-		public void editContact(String firstName, String lastName)
-		{
-		if (firstName.Equals(person.getFirstName()) && lastName.Equals(person.getLastName()))
-		{
-			Console.WriteLine("Enter address : ");
-			person.setAddress(Console.ReadLine());
-			Console.WriteLine("Enter City  : ");
-			person.setCity(Console.ReadLine());
-			Console.WriteLine("Enter state  : ");
-			person.setState(Console.ReadLine());
-			Console.WriteLine("Enter Mobile Number  : ");
-			person.setPhoneNumber(Console.ReadLine());
-			Console.WriteLine("Enter Email Id : ");
-			person.setEmail(Console.ReadLine());
-			Console.WriteLine("Contact updated successfully\n");
-		}
-		else
-		{
-			Console.WriteLine("name not found");
-		}
+	// TO ADD NEW CONTACT
+	public void addContact()
+	{
+		Console.WriteLine("Enter FirstName : ");
+		person.setFirstName(Console.ReadLine());
+		Console.WriteLine("Enter LastName : ");
+		person.setLastName(Console.ReadLine());
+		Console.WriteLine("Enter address : ");
+		person.setAddress(Console.ReadLine());
+		Console.WriteLine("Enter City  : ");
+		person.setCity(Console.ReadLine());
+		Console.WriteLine("Enter state  : ");
+		person.setState(Console.ReadLine());
+		Console.WriteLine("Enter Mobile Number:");
+		person.setPhoneNumber(Console.ReadLine());
+		Console.WriteLine("Enter Email Id : ");
+		person.setEmail(Console.ReadLine());
 
 		Console.WriteLine(person);
 	}
-}
+		// DELETE CONTACT DETAIL BY PERSON NAME
+		public void deleteContactDetail(String firstName, String lastName)
+		{
+			if (firstName.Equals(person.getFirstName()) && lastName.Equals(person.getLastName()))
+			{
+				person = null;
+				Console.WriteLine("Contact deleted successfully\n");
+			}
+			else
+			{
+				Console.WriteLine("name does not exist");
+			}
+			Console.WriteLine("deleted",person);
+		}
+	}
+
 
 
